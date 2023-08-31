bef1f1dbc9d1927bc0774a98ed73c0a253dbc042
bijan75 <50327729+bijan75@users.noreply.github.com>
2023-08-31 17:20:16 +0100

    Final project

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { AiFillInstagram, AiFillYoutube, AiFillGithub } from "react-icons/ai";
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>Burger Shop</h2>
+        <p>Nous nous efforçons de vous offrir le meilleur goût possible.</p>
+        <br />
+        <em>Vos commentaires sont les bienvenus.</em>
+        <strong>Tous droits réservés @burgershop</strong>
+        <br />
+        <strong>Cree par Najib EL MECHMACHI</strong>
+      </div>
+      <aside>
+        <h4>Suivez nous sur</h4>
+        <div>
+          <a href="https://youtube.com/xyz">
+            <AiFillYoutube />
+          </a>
+          <a href="https://instagram.com/xyz">
+            <AiFillInstagram />
+          </a>
+        </div>
+      </aside>
+    </footer>
+  );
+};
+export default Footer;
